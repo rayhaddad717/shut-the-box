3bfe475c7ce0521a8aabef063197e6a9e8cdcf5d
Ray Haddad <rayghaddad@gmail.com>
2024-04-21 20:03:22 +0300

    og image, no ssr

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import Game from "../components/game";
+import dynamic from "next/dynamic";
+
+const GameComponent = dynamic(() => import("../components/game"), {
+  ssr: false,
+});
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Game />
+      <GameComponent />
     </div>
   );
 }
